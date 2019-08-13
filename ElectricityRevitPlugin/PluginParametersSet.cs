45f5a1be187301a4e616d4547c84a30fb92e3427
Евгений Ивачёв <evgivachev@gmail.com>
2019-08-13 16:55:13 +0300

    1

--- ElectricityRevitPlugin/PluginParametersSet.cs
+++ ElectricityRevitPlugin/PluginParametersSet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace ElectricityRevitPlugin
 {
-    class PluginParametersSet
+    abstract class PluginParametersSet
     {
-
+        protected abstract Dictionary<string, ExternalDefinition> Parameters { get; }
+          
     }
 }
