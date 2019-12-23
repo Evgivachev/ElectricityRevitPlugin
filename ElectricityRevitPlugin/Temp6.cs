52a21807afbc47285c23ad2f633f1163e6ba3fbf
Евгений Ивачёв <evgivachev@gmail.com>
2019-12-23 15:20:16 +0300

    1

---
+++ ElectricityRevitPlugin/Temp6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.UI;
+
+namespace ElectricityRevitPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Temp6 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var app = uiApp.Application;
+            var doc = uiDoc.Document;
+            var result = Result.Succeeded;
+            var br = BrowserOrganization.GetCurrentBrowserOrganizationForSheets(doc);
+            var q = br.GetFolderItems(new ElementId(22618482));
+           
+            return result;
+
+        }
+    }
+}
