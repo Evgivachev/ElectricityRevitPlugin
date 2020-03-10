0a6ca79362535d541a5c59718e109c9d52767874
Евгений Ивачёв <evgivachev@gmail.com>
2020-03-10 14:50:41 +0300

    1

---
+++ DuctSystemRevitPlugin/DuctSystemExternalApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.UI;
+using VCRevitRibbonUtilCustom;
+
+namespace DuctSystemRevitPlugin
+{
+    class DuctSystemExternalApp : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication uicApp)
+        {
+            var result = Result.Succeeded;
+            try
+            {
+                MyRibbon.GetApplicationRibbon(uicApp)
+                    .Tab("ОВиК")
+                    .Panel("Виды")
+                    .CreateButton<CreateViewsExternalCommand>("Создание видов для вентиляционных систем", "Создание видов",
+                        bt =>
+                            bt.SetLargeImage(Resource1.icons8_гомер_симпсон_32)
+                                .SetLongDescription<MyButton>("Создание видов для вентиляционных систем")
+                                .SetContextualHelp<MyButton>(ContextualHelpType.Url,
+                                    "https://www.revitapidocs.com/2019/"));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message + "\n" + e.StackTrace);
+                return Result.Failed;
+            }
+            return result;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
