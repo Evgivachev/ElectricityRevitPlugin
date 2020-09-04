93f7ab1ad180395338e222d9c3ca39c14286fe67
Евгений Ивачёв <evgivachev@gmail.com>
2020-09-04 17:39:58 +0300

    123

---
+++ ElectricityRevitPlugin/CopyElementsInSameViewSchedule/CopyElementsInSameScheduleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ElectricityRevitPlugin.CopyElementsInSameViewSchedule
+{
+    public class CopyElementsInSameScheduleViewModel
+    {
+        private readonly Document _doc;
+        public CopyElementsInSameScheduleView(Document doc)
+        {
+            _doc = doc;
+            var activeView = _doc.ActiveView as ViewSchedule;
+            if (activeView is null)
+                throw new InvalidOperationException("Вам следует перейти на вид ключевой спецификации");
+            Elements = new ObservableCollection<Element>(new FilteredElementCollector(_doc, activeView.Id)
+               );
+        }
+        public ObservableCollection<Element> Elements { get; set; }
+    }
+}
---
+++ ElectricityRevitPlugin/CopyElementsInSameViewSchedule/ExternalCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ElectricityRevitPlugin.CopyElementsInSameViewSchedule
+{
+    public class ExternalCommand : DefaultExternalCommand
+
+    {
+        protected override Result DoWork(ref string message, ElementSet elements)
+        {
+            
+
+
+
+        }
+    }
+}
