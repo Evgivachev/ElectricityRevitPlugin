a2687c8943ad3f5d7d49ce3549699b36f689e342
Evgivachev <evgivachev@gmail.com>
2024-09-07 17:16:13 +0300

    cicd

--- build/Build.cs
+++ build/Build.cs
@@ -1,24 +1,33 @@
 using Bimlab.Nuke.Components;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
 using RxBim.Nuke.Builders;
 using RxBim.Nuke.Builds;
 using RxBim.Nuke.Revit.Generators;
 
 [ShutdownDotNetAfterServerBuild]
+[GitHubActions(name: "Compile",
+    image: GitHubActionsImage.Ubuntu2004,
+    On = new[] { GitHubActionsTrigger.Push },
+    InvokedTargets = new[] { nameof(BuildApp) }
+)]
 class Build : RxBimBuild<RevitInstallerBuilder2021, RevitPackageContentsGenerator, RevitProjectPropertiesGenerator, OptionsBuilder>,
     IRestore,
     IHazConfiguration
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-    /// <summary>Revit Version.</summary>
-    [Parameter(null)]
+    [Parameter("Revit Version.")]
     public string RevitVersion { get; set; } = "2022";
+
     public static int Main() => Execute<Build>(x => x.Compile);
+
+    Target BuildApp => d => d
+        .Produces(RootDirectory / "out/*")
+        .Inherit(t =>
+        {
+            Project = "ElectricityRevitPluginApp";
+            return t;
+        }, BuildInnoExe);
 
 
     T From<T>()
